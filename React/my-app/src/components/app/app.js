6873a805360584cc83e90b9a1ea31631570d332c
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-16 19:26:55 +0300

    Create component FirstComponent and import code  in this component

---
+++ React/my-app/src/components/app/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import FirstComponent from "../FirstComponent/FirstComponent";
+
+export default class App extends Component {
+  render(){
+    return(
+      <FirstComponent/>
+    )
+  }
+}
