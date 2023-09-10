2097c1f98c6821b38e82fae79467455c313020af
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-09-10 20:52:39 +0300

    create function calculateWinner

--- React/my-app/src/components/game/broad.jsx
+++ React/my-app/src/components/game/broad.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Square from "./square";
-import "./style.css"
+import "./style.css";
 
 
 export default class Board extends React.Component {
@@ -55,4 +55,25 @@ renderSquare(i) {
           </div>
         );
       }
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for(let i = 0; i<lines.length;i++){
+    const [a, b, c] = lines[i]
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+  
 }
