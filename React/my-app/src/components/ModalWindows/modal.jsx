e53825e369f83f0fec8e66cee6ed4e9f5637b3b6
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-11-14 23:08:44 +0200

    ModalWindows part 1

--- React/my-app/src/components/ModalWindows/modal.jsx
+++ React/my-app/src/components/ModalWindows/modal.jsx
@@ -1,13 +1,68 @@
 import React from "react";
 import styled from "styled-components";
+import Portal  from "./portal";
+import PropTypes from 'prop-types';
 
+const ModalOverlay = styled.div`
+`
+const ModalWindow = styled.div`
+`
+const ModalHeader = styled.div`
+`
+const ModalBody = styled.div`
+`
+const ModalFooter = styled.div`
+`
+const ModalTitle = styled.div`
+`
+const Icon = styled.button``
 
-export default class ModalWindows extends React.Component{
-    render(){
+const FooterButton = styled.button`
+`
+
+const ModalWindows = (
+    //title,isOpen,onCancel,onSubmit,children
+) =>{
+
         return(
         <>
         <h1>Modal windows</h1>
+        <Portal>
+            <ModalOverlay>
+                <ModalWindow>
+                <ModalHeader>
+                    <ModalTitle>
+                        {/* {title}
+                    <Icon name='times' onClick={onCancel}></Icon> */}
+                    </ModalTitle>
+                </ModalHeader>
+
+                <ModalBody>
+
+                </ModalBody>
+
+                <ModalFooter>
+                    {/* <FooterButton onClick={onCancel} invert>Cancel</FooterButton>
+                    <FooterButton onClick={onCancel}>Submit</FooterButton> */}
+                </ModalFooter>
+
+                </ModalWindow>
+            </ModalOverlay>
+        </Portal>
         </>
-        )
-    }
-}
+        )  
+};
+// ModalWindow.propTypes ={
+//     title: PropTypes.string,
+//     isOpen: PropTypes.bool,
+//     onCancel:PropTypes.func,
+//     onSubmit:PropTypes.func,
+//     children:PropTypes.node,
+// };
+// ModalWindow.defaultProps={
+//     title:'Modal title',
+//     isOpen:false,
+//     onCancel:()=>{},
+//     onSubmit:()=>{},
+// }
+export default ModalWindows
