934f57509d8b206f1bf7b1fb56928cae979b7eb8
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-11-15 23:08:02 +0200

    Create component webDevComponents

--- React/my-app/src/components/webDevComponents/webDevComponents.jsx
+++ React/my-app/src/components/webDevComponents/webDevComponents.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import styled from "styled-components";
-
+//import styled from "styled-components";
+import SandBoxButton from "./components/button/sandBoxButton";
 
 export default class DevComponetns extends React.Component{
     render(){
         return(
             <> 
                 <h1>Course components cours WebDev</h1> 
+
+                <h2> Button</h2>
+                <SandBoxButton></SandBoxButton>
             </>
         )
     }
