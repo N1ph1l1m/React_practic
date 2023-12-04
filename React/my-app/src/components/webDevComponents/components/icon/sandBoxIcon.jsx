2e42e5650f69e9bfddff3254036a8a8d64066052
N1ph1l1m <zerorad@inbox.ru>
2023-12-04 17:13:50 +0200

    Work with icons part 1

--- React/my-app/src/components/webDevComponents/components/icon/sandBoxIcon.jsx
+++ React/my-app/src/components/webDevComponents/components/icon/sandBoxIcon.jsx
@@ -1,12 +1,36 @@
 import React, { Fragment } from 'react';
 import Icon from './icon';
 import Button from "../button/button";
-// import ButtonFroup from '../groupButton/groupButton';
+import ButtonGroup from '../groupButton/groupButton';
 
 const SandBoxIcon = () => (
     <Fragment>
     <h2><span>1. Base icon:</span></h2>
-<Icon/>
+<Icon size={2} onClick={()=>{ console.log('!!!');}} name="atom"/>
+    
+    <h2><span>2. Icon button  group:</span></h2>
+    <ButtonGroup>
+        <Button active>
+            <Icon name="align-right"/>
+        </Button>
+        <Button active>
+            <Icon name="align-center"></Icon>
+        </Button>
+        <Button>
+        <Icon name="align-left" />
+      </Button>
+      <Button>
+        <Icon name="align-justify" />
+      </Button>
+    </ButtonGroup>
+
+    <h2><span>3. Icon with size 8rem</span></h2>
+    <Icon title="badge" size={8} name="award" />
+
+    <h2><span>4. Disabling icon functionality:</span></h2>
+    <Icon size={2} disabled onClick={() => { console.log('!!!'); }} name="atom" />
+
+    
     </Fragment>
 );
 export default SandBoxIcon;
