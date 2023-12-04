6e7059042c85943a01c354de412faa2b57644d37
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-12-05 01:11:00 +0200

    Add react icons

--- React/my-app/src/components/webDevComponents/components/icon/sandBoxIcon.jsx
+++ React/my-app/src/components/webDevComponents/components/icon/sandBoxIcon.jsx
@@ -1,36 +1,49 @@
 import React, { Fragment } from 'react';
-import Icon from './icon';
+//import Icon from './icon';
 import Button from "../button/button";
 import ButtonGroup from '../groupButton/groupButton';
+import { AiFillApple,AiFillAppstore,AiFillAlipayCircle } from "react-icons/ai";
+import './icon.css'
+import styled from "styled-components";
+
+const SandBoxIconWrap = styled.div`
+display:flex;
+justify-content: center;
+flex-flow: column wrap;
+
+`
 
 const SandBoxIcon = () => (
-    <Fragment>
-    <h2><span>1. Base icon:</span></h2>
-<Icon size={2} onClick={()=>{ console.log('!!!');}} name="atom"/>
+  <SandBoxIconWrap>
+   <h2><span>1. Base icon:</span></h2>
+   <Button className="buttonSize" active>
+        <AiFillAlipayCircle  size={44}/>Simple Button 
+    </Button>
     
-    <h2><span>2. Icon button  group:</span></h2>
-    <ButtonGroup>
-        <Button active>
-            <Icon name="align-right"/>
-        </Button>
-        <Button active>
-            <Icon name="align-center"></Icon>
-        </Button>
-        <Button>
-        <Icon name="align-left" />
-      </Button>
-      <Button>
-        <Icon name="align-justify" />
-      </Button>
-    </ButtonGroup>
+<h2><span>2. Icon button  group:</span></h2>
 
-    <h2><span>3. Icon with size 8rem</span></h2>
-    <Icon title="badge" size={8} name="award" />
+<ButtonGroup className="buttonGroupSize" vertical >
+    <Button className="buttonSize" active>
+        <AiFillApple  size={44}/>Apple Button 
+    </Button>
 
-    <h2><span>4. Disabling icon functionality:</span></h2>
-    <Icon size={2} disabled onClick={() => { console.log('!!!'); }} name="atom" />
+    <Button className="buttonSize">
+        <AiFillAppstore size={44} />Windows Button
+    </Button>
+</ButtonGroup>
+
+<h2><span>3. Icon with size 8rem</span></h2>
+
+
+<h2><span>4. Disabling icon functionality:</span>
+<Button className="buttonSize" disabled>
+        <AiFillAppstore size={44} />Windows Button
+    </Button></h2>
+  </SandBoxIconWrap>
+ 
+
 
     
-    </Fragment>
+   
 );
 export default SandBoxIcon;
